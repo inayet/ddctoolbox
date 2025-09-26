757cbc2ab75b014c9951cd4e2b562c18c34831ad
inayet <inayet@dreamsapi.com>
2025-09-26 17:37:34 -0600

    hash updated

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
           owner = "timschneeb";
           repo = "DDCToolbox";
           rev = "master";
-          sha256 = "sha256-NqhSMfIAnpJcJ8qTSV61tbiCKoI+INfoknTl5/4g7h4=";
+          sha256 = "NqhSMfIAnpJcJ8qTSV61tbiCKoI+INfoknTl5/4g7h4=";
         };
 
         ddctoolbox = pkgs.stdenv.mkDerivation {

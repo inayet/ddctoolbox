7b38d051a237b083441e879255bad6fce09c8c99
inayet <inayet@dreamsapi.com>
2025-09-26 14:40:47 -0600

    u

--- flake.nix
+++ flake.nix
@@ -1,22 +1,17 @@
 {
   description = "DDCToolbox built with Qt5 and qmake";
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
-
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-    }:
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-
+      system: let
+        pkgs = import nixpkgs {inherit system;};
         ddctoolbox-src = pkgs.fetchgit {
           url = "https://github.com/ThePBone/DDCToolbox.git";
           rev = "master"; # Consider pinning a commit for reproducibility
@@ -25,9 +20,7 @@
         ddctoolbox = pkgs.stdenv.mkDerivation {
           pname = "ddctoolbox";
           version = "unstable";
-
           src = ddctoolbox-src;
-
           nativeBuildInputs = with pkgs.qt5; [
             qmake
             wrapQtAppsHook
@@ -36,12 +29,10 @@
             qt5.qtbase
             libGL
           ];
-
           installPhase = ''
-
             mkdir -p $out/bin
             find . -type f -name DDCToolbox -exec cp {} $out/bin/ddctoolbox \;
-                        
+
             mkdir -p $out/share/applications
             cat > $out/share/applications/ddc_toolbox.desktop <<EOF
             [Desktop Entry]
@@ -58,32 +49,26 @@
             EOF
 
             mkdir -p $out/share/pixmaps
-            cp img/icon.png $out/share/pixmaps/ddc-toolbox.png
+            if [ -f img/icon.png ]; then
+              cp img/icon.png $out/share/pixmaps/ddc-toolbox.png
+            fi
           '';
-
           meta = with pkgs.lib; {
             description = "Create and edit DDCs on Linux";
             homepage = "https://github.com/ThePBone/DDCToolbox";
             license = licenses.gpl3Plus;
             maintainers = [
-
               "inayet"
             ];
             platforms = platforms.linux;
           };
         };
-
-      in
-      {
+      in {
         packages.default = ddctoolbox;
-
         apps.default = {
           type = "app";
-          program = "${pkgs.writeShellScript "ddctoolbox-wrapper" ''
-            exec ${ddctoolbox}/bin/ddctoolbox "$@"
-          ''}";
+          program = "${ddctoolbox}/bin/ddctoolbox";
         };
-
         devShells.default = pkgs.mkShell {
           buildInputs = with pkgs; [
             qt5.qmake

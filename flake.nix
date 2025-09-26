193b718373f53a9fc41f14bfadf75764c0f56423
inayet <inayet@dreamsapi.com>
2025-09-26 16:41:58 -0600

    hash updated

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "DDCToolbox built with Qt5 and qmake";
+  description = "DDCToolbox built with Qt6 and qmake";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -14,84 +14,172 @@
       system:
       let
         pkgs = import nixpkgs { inherit system; };
-
         desktopFile = pkgs.writeText "ddc_toolbox.desktop" ''
           [Desktop Entry]
           Name=DDC Toolbox
           GenericName=DDC Editor
           Comment=Create and edit DDCs on Linux
-          Keywords=editor
-          Categories=AudioVideo;Audio;Editor
+          Keywords=editor;audio;ddc
+          Categories=AudioVideo;Audio;Editor;
           Exec=ddctoolbox
           Icon=ddc-toolbox
           StartupNotify=false
           Terminal=false
           Type=Application
+          MimeType=application/x-ddc;
         '';
-
         ddctoolbox-src = pkgs.fetchFromGitHub {
           owner = "timschneeb";
           repo = "DDCToolbox";
           rev = "master";
-          sha256 = "sha256-NqhSMfIAnpJcJ8qTSV61tbiCKoI+INfoknTl5/4g7h4="; # pkgs.lib.fakeHash;
+          sha256 = "sha256-NqhSMfIAnpJcJ8qTSV61tbiCKoI+INfoknTl5/4g7h4=";
         };
         ddctoolbox = pkgs.stdenv.mkDerivation {
           pname = "ddctoolbox";
-          version = "unstable";
+          version = "unstable-2024-09-26";
           src = ddctoolbox-src;
-          nativeBuildInputs = with pkgs.qt6; [
-            qmake
-            wrapQtAppsHook
-
+          
+          # Fix locale issues during build
+          env = {
+            LANG = "C.UTF-8";
+            LC_ALL = "C.UTF-8";
+          };
+          
+          nativeBuildInputs = with pkgs; [
+            qt6.qmake
+            qt6.wrapQtAppsHook
+            pkg-config
           ];
+          
           buildInputs = with pkgs; [
-            kdePackages.qtbase
-            kdePackages.qttools # For Qt Designer, tools
-            kdePackages.qt5compat
-            kdePackages.qtsvg
-            kdePackages.qt6ct # If the app uses SVG icons
+            qt6.qtbase
+            qt6.qttools
+            qt6.qt5compat
+            qt6.qtsvg
             libGL
             utf8cpp
+            # Additional dependencies that might be needed
+            alsa-lib
+            pulseaudio
           ];
+          
+          # Configure phase to set up proper qmake flags
+          configurePhase = ''
+            runHook preConfigure
+            
+            # Set up Qt environment
+            export QT_SELECT=6
+            
+            # Run qmake with proper flags
+            qmake PREFIX=$out \
+                  CONFIG+=release \
+                  CONFIG+=c++17 \
+                  DDCToolbox.pro
+            
+            runHook postConfigure
+          '';
+          
+          buildPhase = ''
+            runHook preBuild
+            
+            # Build with parallel jobs
+            make -j$NIX_BUILD_CORES
+            
+            runHook postBuild
+          '';
+          
           installPhase = ''
+            runHook preInstall
+            
+            # Install the binary
             mkdir -p $out/bin
-            find . -type f -name DDCToolbox -exec cp {} $out/bin/ddctoolbox \;
-                        
+            
+            # Find and install the executable
+            find . -type f -name "DDCToolbox" -executable -exec cp {} $out/bin/ddctoolbox \;
+            find . -type f -name "ddctoolbox" -executable -exec cp {} $out/bin/ddctoolbox \;
+            
+            # Ensure we have an executable
+            if [ ! -f "$out/bin/ddctoolbox" ]; then
+              echo "Error: DDCToolbox executable not found!"
+              find . -name "*DDC*" -type f
+              exit 1
+            fi
+            
+            # Install desktop file
             mkdir -p $out/share/applications
             cp ${desktopFile} $out/share/applications/ddc_toolbox.desktop
-
+            
+            # Install icon if available
             mkdir -p $out/share/pixmaps
             if [ -f img/icon.png ]; then
               cp img/icon.png $out/share/pixmaps/ddc-toolbox.png
+            elif [ -f src/img/icon.png ]; then
+              cp src/img/icon.png $out/share/pixmaps/ddc-toolbox.png
+            elif [ -f assets/icon.png ]; then
+              cp assets/icon.png $out/share/pixmaps/ddc-toolbox.png
             fi
+            
+            # Install any additional resources
+            if [ -d resources ]; then
+              mkdir -p $out/share/ddctoolbox
+              cp -r resources/* $out/share/ddctoolbox/
+            fi
+            
+            runHook postInstall
           '';
+          
+          # Enable debug symbols for troubleshooting if needed
+          separateDebugInfo = true;
+          
           meta = with pkgs.lib; {
-            description = "Create and edit DDCs on Linux";
-            homepage = "https://github.com/ThePBone/DDCToolbox";
+            description = "Create and edit DDCs (Digital Dynamic Range Compression) files on Linux";
+            longDescription = ''
+              DDCToolbox is a tool for creating and editing DDC (Digital Dynamic Range Compression) 
+              files on Linux. It provides a graphical interface for managing audio processing parameters.
+            '';
+            homepage = "https://github.com/timschneeb/DDCToolbox";
             license = licenses.gpl3Plus;
-            maintainers = [
-              "timschneeb"
-              "inayet"
-            ];
+            maintainers = with maintainers; [ ];
             platforms = platforms.linux;
+            mainProgram = "ddctoolbox";
           };
         };
       in
       {
-        packages.default = ddctoolbox;
+        packages = {
+          default = ddctoolbox;
+          ddctoolbox = ddctoolbox;
+        };
+        
         apps.default = {
           type = "app";
           program = "${ddctoolbox}/bin/ddctoolbox";
         };
+        
         devShells.default = pkgs.mkShell {
+          inputsFrom = [ ddctoolbox ];
           buildInputs = with pkgs; [
-            kdePackages.qtbase
-            kdePackages.qttools # For Qt Designer, tools
-            kdePackages.qtsvg
-            kdePackages.qt6ct # If the app uses SVG icons
+            # Development tools
+            gdb
+            valgrind
+            qtcreator
+            # Qt development tools
+            qt6.qtbase
+            qt6.qttools
+            qt6.qt5compat
+            qt6.qtsvg
             libGL
             utf8cpp
+            pkg-config
           ];
+          
+          shellHook = ''
+            echo "DDCToolbox development environment"
+            echo "Qt version: $(qmake -version)"
+            export QT_SELECT=6
+            export LANG=C.UTF-8
+            export LC_ALL=C.UTF-8
+          '';
         };
       }
     );

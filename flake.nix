52c2a499b9e9f72f9f9dafb898d03dde702b8ca5
inayet <inayet@dreamsapi.com>
2025-09-26 13:55:01 -0600

    u

---
+++ flake.nix
@@ -0,0 +1,84 @@
+{
+  description = "DDCToolbox built with Qt5 and qmake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        ddctoolbox-src = pkgs.fetchgit {
+          url = "https://github.com/ThePBone/DDCToolbox.git";
+          rev = "master"; # Consider pinning a commit for reproducibility
+          sha256 = "sha256-00mg0hry3ysdgjrah9gsmvhlsvldrsnxnjjrbx52al30s8nkg3rf";
+        };
+
+        ddctoolbox = pkgs.stdenv.mkDerivation {
+          pname = "ddctoolbox";
+          version = "unstable";
+
+          src = ddctoolbox-src;
+
+          nativeBuildInputs = with pkgs.qt5; [
+            qmake
+            wrapQtAppsHook
+          ];
+
+          buildInputs = with pkgs; [
+            qt5.qtbase
+            libGL
+          ];
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp ./DDCToolbox $out/bin/ddctoolbox
+
+            # Optional: install desktop entry and icon
+            mkdir -p $out/share/applications
+            cat > $out/share/applications/ddc_toolbox.desktop <<EOF
+            [Desktop Entry]
+            Name=DDC Toolbox
+            GenericName=DDC Editor
+            Comment=Create and edit DDCs on Linux
+            Keywords=editor
+            Categories=AudioVideo;Audio;Editor
+            Exec=ddctoolbox
+            Icon=ddc-toolbox
+            StartupNotify=false
+            Terminal=false
+            Type=Application
+            EOF
+
+            mkdir -p $out/share/pixmaps
+            cp img/icon.png $out/share/pixmaps/ddc-toolbox.png
+          '';
+        };
+      in
+      {
+        packages.default = ddctoolbox;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = ddctoolbox;
+          exePath = "/bin/ddctoolbox";
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            qt5.qmake
+            qt5.qtbase
+            libGL
+          ];
+        };
+      }
+    );
+}
